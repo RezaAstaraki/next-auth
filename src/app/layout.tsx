a50e1cffc8a56c944dde7afed3b5e97f392d73c8
reza <reza.astaraky@gmail.com>
2025-08-14 21:03:18 +0330

    add app name and version to footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata, Viewport } from "next";
 import { Link } from "@heroui/link";
 import clsx from "clsx";
 
-
+import { version, name } from "@/package.json";
 
 import { siteConfig } from "@/src/config/site";
 import { fontSans } from "@/src/config/fonts";
@@ -41,30 +41,23 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen text-foreground bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
-        <NextUiProviders themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        <NextUiProviders
+          themeProps={{ attribute: "class", defaultTheme: "dark" }}
+        >
           <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              <TestModal/>
+              <TestModal />
               {children}
-
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
-              </Link>
+            <footer className="w-full flex text-xs items-center justify-center py-3">
+              {name}-version-{version}
             </footer>
           </div>
-          <RootModal/>
+          <RootModal />
         </NextUiProviders>
       </body>
     </html>
